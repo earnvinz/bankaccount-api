b00802ff019490fa547ced45d22cbda70ed70a3c
earnvinz <erb.u@ku.th>
2020-10-03 13:43:53 +0700

    BankAccount Test Postman app

---
+++ src/main/java/th/ac/ku/bankaccount2/Bankaccount2Application.java
@@ -0,0 +1,13 @@
+package th.ac.ku.bankaccount2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bankaccount2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bankaccount2Application.class, args);
+	}
+
+}
---
+++ src/test/java/th/ac/ku/bankaccount2/Bankaccount2ApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.bankaccount2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bankaccount2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
